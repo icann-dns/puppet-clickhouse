85cb842fdd4ee2b4e3b45cda6c667ecfc19a148d
David Soltero <david.soltero@icann.org>
2020-01-24 12:49:55 -0800

    update

--- types/database_filter.pp
+++ types/database_filter.pp
@@ -1,4 +1,1 @@
-type Clickhouse::Database_filter = Struct[{
-  database       => String[1],
-  databasefilter => Array[Clickhouse::Table_filter],
-}]
+type Clickhouse::Database_filter = Array[Clickhouse::Table_filter]
